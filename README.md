102193e3991d8d238a63f0ddf93f4f374a490800
Kevin Wegrowski <kevinw778@gmail.com>
2018-03-29 00:35:09 -0400

    Testing README.md formatting and begrudgingly pythonifying my code.

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
+# Time Tracker
+
 Time Tracker program made to keep track of when an individual should leave
 for the day, if following a 40-hour, 8 hours a day schedule.
 
-Planning on making it so the user can simply have a text file or some kind
-of setup dialog that can adjust how many hours they plan to work each day
-or per week, then make the necessary calculations off of that information,
-instead of using a text file or hard-coded values.
+## Future Dev
+- [x] Finally commit TimeTracker code to my Github
+- [] Create dialog to change hours/day / hours/week settings
+- [] Separate main hours acquisition and backup acquisition into functions so
+I can properly fall back on the backup when the user doesn't have the proper
+data on their clipboard.
--- src/TimeTracker.py
+++ src/TimeTracker.py
@@ -11,47 +11,47 @@ from bs4 import BeautifulSoup
 #import RecipeItem
 
 # Will eventually work on the scraping version
-def scrapeStuff():
+def scrape_stuff():
 
-    pageLink = 'https://www.gameskinny.com/96m7g/kingdom-hearts-birth-by-sleep-aquas-ultimate-command-deck-guide'
-    page = urllib3.urlopen(pageLink)
+    page_link = 'https://www.gameskinny.com/96m7g/kingdom-hearts-birth-by-sleep-aquas-ultimate-command-deck-guide'
+    page = urllib3.urlopen(page_link)
 
     soup = BeautifulSoup(page, 'html.parser') # creates the soup object to work with -- Has one child but many descendants
 
 
 # Converts passed in hours & minutes into Time - hh:mm[am/pm]
-def toTime(hours, minutes):
+def to_time(hours, minutes):
 
     hour = 0
     minute = 0
-    extraHour = 0 # Extra hour for if/when minutes go over 60 in this calc
+    extra_hour = 0 # Extra hour for if/when minutes go over 60 in this calc
 
     if (minutes > 59):
         minute = (minutes - 60)
-        extraHour = 1
+        extra_hour = 1
 
     else:
         minute = minutes
 
     # Honestly most of this is overkill -- input won't generally cross these cases
     if (hours >= 24):
-        hour = 1 + extraHour + (hours - 24)
+        hour = 1 + extra_hour + (hours - 24)
 
     else:
-        hour += (hours + extraHour)
+        hour += (hours + extra_hour)
 
     # Determine AM/PM
-    dayPart = ""
+    day_part = ""
     if (hour > 12):
         hour -= 12
-        dayPart = "pm"
+        day_part = "pm"
     else:
-        dayPart = "am"
+        day_part = "am"
 
     # Add padding for single-digit times when necessary
     pad = ('0' if (minute < 10) else '')
 
-    return ( str(hour) + ':' + ( pad + str(minute) ) + dayPart )
+    return ( str(hour) + ':' + ( pad + str(minute) ) + day_part )
 
 
 ### Main Program ###
@@ -61,24 +61,24 @@ now = datetime.datetime.now()
 day = now.strftime("%A").lower()
 
 # Time separated into Hour & Minutes
-timeHour = now.hour
-timeMinute = now.minute
+time_hour = now.hour
+time_minute = now.minute
 
 
 # Will set this up to read from file, later
-# That will help with using custom targetHours
-targetHoursStandard = {"monday": 8, "tuesday": 16, "wednesday": 24,
+# That will help with using custom target_hours
+target_hours_standard = {"monday": 8, "tuesday": 16, "wednesday": 24,
                        "thursday": 32, "friday": 40, "saturday": 48,
                        "sunday": 56}
 
 # Keeping this for when I implement the "either or" option of data-entry
 # INPUT #
 # Number of hours worked this week
-#hoursWorked = abs( float(input("Enter Your Current Hours Worked\n")) )
+#hours_worked = abs( float(input("Enter Your Current Hours Worked\n")) )
 
 ### INPUT ###
 # Whether or not you've taken your lunch. If you have, it's 0 (no time subtracted from total) else 1
-lunchTaken = ( 0 if ( input("Have you taken your lunch yet? (y/n)\n").lower() == 'y') else 1)
+lunch_taken = ( 0 if ( input("Have you taken your lunch yet? (y/n)\n").lower() == 'y') else 1)
 
 
 ### VARIABLE OVERRIDE ###
@@ -96,28 +96,28 @@ time = reg.findall(clip) # Contains (hh:mm:ss) in a list
 hours = int(time[0])
 minutes = float( int(time[1]) / 60 ) # convert from minutes to decimal
 
-hoursWorked = hours + minutes
+hours_worked = hours + minutes
 
 # Time Remaining - Minutes & Seconds
-timeRemaining = targetHoursStandard[day] - hoursWorked
+time_remaining = target_hours_standard[day] - hours_worked
 
 # Hours Remaining
-#hoursRemaining = int(time[0])
-hoursRemaining = int(timeRemaining)
+#hours_remaining = int(time[0])
+hours_remaining = int(time_remaining)
 
 # Minutes Remaining
-#minutesRemaining = int(time[1])
-minutesRemaining = math.ceil( ( float(timeRemaining % 1) * 60) )
+#minutes_remaining = int(time[1])
+minutes_remaining = math.ceil( ( float(time_remaining % 1) * 60) )
 
 # Target time that the user should leave work for the day
-targetHour = (timeHour + hoursRemaining) + lunchTaken
-targetMinute = timeMinute + minutesRemaining
+target_hour = (time_hour + hours_remaining) + lunch_taken
+target_minute = time_minute + minutes_remaining
 
-targetTime = toTime(targetHour, targetMinute)
+target_time = to_time(target_hour, target_minute)
 
 # This is calculating 5-hours off for some reason... Look into this -
 # simulate the day when I get home
-print("Leave work by: " + targetTime)
+print("Leave work by: " + target_time)
 
 #for n in time:
 #    print (n)
